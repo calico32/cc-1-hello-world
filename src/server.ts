52753750d7c4ba76147b2d69d2f3284ac1d67b29
wiisportsresort <devdoge1@gmail.com>
2020-10-12 09:43:22 -0400

    discord bot version

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,63 @@
+import { Client, GuildMember, Message, MessageEmbed } from 'discord.js';
 import dotenv from 'dotenv';
-import express from 'express';
-import { AddressInfo } from 'net';
-import path from 'path';
 
 import { getCatImage } from './cat';
 
 dotenv.config();
 
-const app = express();
-
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
-
-app.set('views', path.resolve(__dirname, '..', 'views'));
-app.set('view engine', 'ejs');
-
-app.get('*', async (req, res) => {
-  res.render('index', {
-    date: new Date(),
-    imageUrl: await getCatImage(),
-  });
+const client = new Client({
+  disableMentions: 'everyone',
 });
 
-const server = app.listen(process.env.PORT, () => {
-  // preload next image
-  getCatImage();
+// client.user is null because we haven't logged in yet
+let mention: string;
 
-  const address = server.address() as AddressInfo;
-  return console.log(`${address.family} server opened at ${address.address}:${address.port}`);
+client.on('message', async (msg: Message) => {
+  if (msg.author.bot) return;
+  if (msg.author.id == client.user?.id) return;
+  if (!msg.guild) return; // don't respond to DMs
+
+  const guild = msg.guild;
+  const me = guild.me as GuildMember;
+  const channel = guild.channels.cache.get(msg.channel.id);
+
+  if (!channel) return;
+
+  if (msg.mentions.has(me)) {
+    if (!channel.permissionsFor(me)?.has('SEND_MESSAGES')) {
+      if (channel.permissionsFor(me)?.has('ADD_REACTIONS')) {
+        msg.react('❌');
+      }
+      return console.error('no perms to send messages');
+    }
+
+    const embed = new MessageEmbed({
+      author: { name: client.user?.username },
+      title: 'hello world',
+      description: 'cat image',
+      image: { url: await getCatImage() },
+    });
+    msg.channel.send(embed);
+  }
 });
+
+client.on('ready', () => {
+  mention = `<@!${client.user?.id}> `;
+  console.log(`${client.user?.tag} ready`);
+  const setPresence = () => {
+    client.user?.setPresence({
+      activity: {
+        name: `vscode | @${client.user?.username}`,
+        type: 'PLAYING',
+      },
+    });
+  };
+  setPresence();
+  setInterval(setPresence, 1000 * 60 * 10);
+});
+
+client
+  .on('warn', console.warn)
+  .on('error', console.error)
+  .on('disconnect', () => console.log('client disconnected'))
+  .login(process.env.DISCORD_TOKEN);
